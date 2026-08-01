fd1bbe09722e667e00a1020d2414f13226421edc
Fushy <113162221+fushy28@users.noreply.github.com>
2026-08-01 09:55:02 +0700

    Add Simple Yatzy game implementation in C++

---
+++ OOP/Simple Yatzy.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int dice =1;
+  while (dice <= 6) {
+    if (dice <=3) {
+      cout<< "Bad luck! You rolled a " << dice << endl;
+      dice++;
+      } else if (dice <6) {
+        cout << "Congratulations! You rolled a " << dice << endl;
+        dice++;
+      }else {
+        cout << "Yatzy!";
+        dice++;
+      }
+      }
+      return 0;
+      }
